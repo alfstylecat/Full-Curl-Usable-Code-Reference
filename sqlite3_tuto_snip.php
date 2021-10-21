1805d4495d75e19e93b44eb24afa864eed88df2d
alfstylecat <66421004+alfstylecat@users.noreply.github.com>
2021-10-21 19:52:53 +0300

    update sqlite3_tuto_snip.php

--- sqlite3_tuto_snip.php
+++ sqlite3_tuto_snip.php
@@ -1,5 +1,15 @@
 <?
 
+//sqlite3 nesnesi olusturarak sqlite3 veritabani baglantisi aciyoruz
+$db = new SQLite3('test.db');
+
+//sqlite3 exec() veritabanindan sorgu yurutme create,insert,update,delete vb.
+$db->exec("CREATE TABLE Cars(id INTEGER PRIMARY KEY, isim METİN, fiyat INT)");
+$db->exec("INSERT INTO arabalar(ad, fiyat) DEĞERLER('Audi', 52642)");
+
+//veritabaninda tablo olusturma 3 alan id name price integer primary key otomatik olarak arttirma
+$db->exec("CREATE TABLE cars(id INTEGER PRIMARY KEY, name TEXT, price INT)");
+
 //Sqlite veritabanı yedekleme
 $backup = new SQLite3('backup.sqlite');
 $conn->backup($backup);
@@ -9,6 +19,10 @@ $query = $db->exec('UPDATE counter SET views=0 WHERE page="test"');
 if ($query) {
     echo 'İşlem gören satır sayısı: ', $db->changes();
 }
+
+//son eklenen satirin kimligine alma
+$last_row_id = $db->lastInsertRowID();
+echo "The last inserted row Id is $last_row_id";
 
 //veritabanı bağlantısını kapatır
 $db = new SQLite3('mysqlitedb.db');
@@ -21,17 +35,46 @@ while ($row = $results->fetchArray()) {
     var_dump($row);
 }
 
+//sqlite query 2
+$rows = $db->query('SELECT * FROM cars');
+foreach ($rows as $row) {
+    $id = $row->id;
+    $name = $row->name;
+    $price = $row->price;
+    echo "$id $name $price \n";
+}
+
+//sorgu query 2 olusturma ve donen degerleri alma
+$res = $db->query('SELECT * FROM cars');
+while ($row = $res->fetchArray()) {
+    echo "{$row['id']} {$row['name']} {$row['price']} \n";
+}
+
 //tek sonuc döndürme query single
 $db = new SQLite3('mysqlitedb.db');
 var_dump($db->querySingle('SELECT username FROM user WHERE userid=1'));
 print_r($db->querySingle('SELECT username, email FROM user WHERE userid=1',
       true));
+//tek sonuc 2
+$versiyon = $db->querySingle('SELECT SQLITE_VERSION()');
+      
+      
+      
 
 //sqlite enjeksiyon önleme eski olabilir bakılacak
 if (get_magic_quotes_gpc()) {
    $name = sqlite_escape_string($name);
 }
 $result = @$db->query("SELECT * FROM users WHERE username = '{$name}'");
+
+//sqlite escape string guvenlik
+$db = new SQLite3('test.db');
+$sql = "SELECT name FROM cars WHERE name = 'Audi'";
+$escaped = SQLite3::escapeString($sql);
+var_dump($sql);
+var_dump($escaped);
+
+
 
 Not – SQLite sorguları için dizelerinizi alıntılamak için addslashes() KULLANILMAMALIDIR; verilerinizi alırken garip sonuçlara yol açacaktır.
 
